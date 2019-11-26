d13555a8fb2506fe0babe86b61368b42df98aa44
b216007 <53688199+b216007@users.noreply.github.com>
2019-11-26 18:25:05 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var pageSize=100,temp=[],initial=0,myFavourite=null
+var pageSize=100,temp=[],initial=0,myFavourite=null,isFound=false,tempFound=[],fInitial=0
 
 function onload() {
 
@@ -19,7 +19,7 @@ function onload() {
 }
 
 function searchItem(value) {
-
+	isFound=false
 	$.ajax({
 		type:'get',
 		url:'https://vast-shore-74260.herokuapp.com/banks',
@@ -38,7 +38,7 @@ function searchItem(value) {
 
 
 function plotData(data,init=0,j=null) {
-	console.log(data,init,j)
+	//console.log(data,init,j)
 	var header=$('#bank-list .header').html()
 	$('#bank-list').empty().append('<tr class="header">'+header+'</tr>')
 	if (!j) {
@@ -65,17 +65,29 @@ function plotData(data,init=0,j=null) {
 			'<td>'+data[i].state+'</td>'+
 			'</tr>'
 		)
-		initial=i+1
+		if(isFound) {
+			fInitial=i+1
+		} else {
+			initial=i+1
+		}
 	}
 }
 function searchByName(value) {
-	if (value.length>2) {
-		searchItem(value)
+	if (value.trim().length>2) {
+		searchLocal(value.trim())
+	}
+
+	if (value.length==0) {
+		plotData(temp,initial,initial+pageSize)
 	}
 }
 
 function nextPage() {
-	plotData(temp,initial,initial+pageSize)
+	if(isFound) {
+		plotData(tempFound,fInitial,fInitial+pageSize)	
+	} else {
+		plotData(temp,initial,initial+pageSize)
+	}
 }
 
 function addFav(i) {
@@ -108,4 +120,15 @@ function viewFavourite() {
 	}
 	temp=banks
 	plotData(banks,0,pageSize)
+}
+
+function searchLocal(query) {
+	var found = [];
+    for (var i = 0; i < temp.length; i++) {
+      if (temp[i].city.toLowerCase().indexOf(query.toLowerCase()) >= 0 || temp[i].branch.toLowerCase().indexOf(query.toLowerCase()) >= 0 || temp[i].state.toLowerCase().indexOf(query.toLowerCase()) >= 0 || temp[i].ifsc.toLowerCase().indexOf(query.toLowerCase()) >= 0 || temp[i].district.toLowerCase().indexOf(query.toLowerCase()) >= 0 || temp[i].address.toLowerCase().indexOf(query.toLowerCase()) >= 0) found.push(temp[i]);
+    }
+   	isFound=true
+   	tempFound=found
+   	fInitial=0
+    plotData(tempFound)
 }
